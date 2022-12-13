0c03679d8fc5a590b83de2ee8a1c0907586260b5
yogesh121415 <ayogesh1214@gmail.com>
2022-12-13 18:29:33 +0530

    Updates

---
+++ precache-manifest.e09df26342c0a598668fa135d5d748f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c2b277777e3ae7882c7",
+    "url": "/my-app/static/css/main.ff947ce8.chunk.css"
+  },
+  {
+    "revision": "4c2b277777e3ae7882c7",
+    "url": "/my-app/static/js/main.4c2b2777.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/my-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/my-app/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "99b7f12dffb4a4f3de0c",
+    "url": "/my-app/static/js/runtime~main.99b7f12d.js"
+  },
+  {
+    "revision": "b5bca59627db13fddc6dabeba8364bd7",
+    "url": "/my-app/index.html"
+  }
+];
